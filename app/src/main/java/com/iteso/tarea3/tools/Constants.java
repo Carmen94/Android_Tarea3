0841e3206cb2ab5f92a1278ae7bb4a34842f722b
Carmen <carmen@RIVERA13>
2019-03-08 11:48:41 -0600

    Finished tarea 3

---
+++ app/src/main/java/com/iteso/tarea3/tools/Constants.java
@@ -0,0 +1,11 @@
+package com.iteso.tarea3.tools;
+
+public class Constants {
+
+    public final static String PRODUCT = "ITEM";
+
+    public final static int EDIT_ACTIVITY = 1;
+
+    public final static int CANCEL_ACTIVITY = 2;
+
+}
